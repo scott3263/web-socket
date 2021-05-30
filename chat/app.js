7b30b505057e9e02386924e61d6b43f3aa6b3dc2
scott <scott3263.park@gmail.com>
2021-05-30 20:56:52 +0900

    first

---
+++ chat/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const utils = require("util");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+const moment = require("moment");
+
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname,"src")));
+
+const PORT = 3200;
+
+io.on("connection", function(socket)  {
+    socket.on("chatting", function(data) {
+        //utils.log(data);
+        const {name, msg} = data;
+
+        io.emit("chatting", {
+            name: name,
+            msg: msg,
+            time: moment(new Date()).format("h:mm:ss A")
+        });
+    });
+});
+
+server.listen(PORT, function(){
+    utils.log(`Server Start! : ${PORT}`);
+});
+
